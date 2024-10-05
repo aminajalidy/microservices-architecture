ad2b871a0a0fd2c31b13cec598de5378d6666189
aminajalidy <jalidyamina1@gmail.com>
2024-10-05 12:28:49 +0200

    Ajout des répertoires config-server, eureka-server et zuul-server sans sous-modules

---
+++ zuul-server/src/main/java/com/microservices/zuulserver/Filters/ZuulServerApplication.java
@@ -0,0 +1,18 @@
+package com.microservices.zuulserver.Filters;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+
+
+public class ZuulServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServerApplication.class, args);
+    }
+
+}
